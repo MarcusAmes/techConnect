93ec3e5de2676eae6b105b15297b5c5cf967c63b
Marcus Ames <marcusames00@gmail.com>
2018-11-29 16:05:28 -0700

    small changes

--- db/seeds/04_connections.js
+++ db/seeds/04_connections.js
@@ -7,7 +7,7 @@ exports.seed = function(knex, Promise) {
       return knex('connections').insert([
         {match_one_user_id: 1, match_two_user_id: 5, connected: true},
         {match_one_user_id: 2, match_two_user_id: 6, connected: true},
-        {match_one_user_id: 3, match_two_user_id: 7, connected: true},
+        {match_one_user_id: 3, match_two_user_id: 6, connected: true},
         {match_one_user_id: 1, match_two_user_id: 6, connected: true}
       ]);
     });
