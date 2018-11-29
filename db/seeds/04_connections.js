72f5572d8fa83ff5907779f2b4a5fbed88970457
unknown <lucasromero65@yahoo.com>
2018-11-29 16:07:18 -0600

    pushing

--- db/seeds/04_connections.js
+++ db/seeds/04_connections.js
@@ -5,6 +5,10 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('connections').insert([
+        {match_one_user_id: 1, match_two_user_id: 5, connected: true},
+        {match_one_user_id: 2, match_two_user_id: 6, connected: true},
+        {match_one_user_id: 3, match_two_user_id: 7, connected: true},
+        {match_one_user_id: 1, match_two_user_id: 6, connected: true}
       ]);
     });
 };
