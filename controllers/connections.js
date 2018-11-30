b90606328f47a09ddc66e68090108859f49d42cf
Marcus Ames <marcusames00@gmail.com>
2018-11-30 08:53:30 -0700

    last minute bug fix and css

--- controllers/connections.js
+++ controllers/connections.js
@@ -3,23 +3,19 @@ const knex = require("../db/knex.js");
 module.exports = {
   yes: (req, res) =>{
     knex('connections').where('match_two_user_id', req.session.user.id).then((second) => {
+      let alreadyMatch = false;
       if (second.length >= 1) {
         for(let i = 0; i < second.length; i++) {
-          if(second[i]['match_one_user_id'] == req.params.id) {
+          if(second[i]['match_one_user_id'] == req.params.id ){
+            alreadyMatch = true;
             knex('connections').update({connected: true}).where('id', second[i].id).then(() => {
               res.redirect('/dashboard')
               return;
             })
-          } else {
-            knex('connections').insert({
-              match_one_user_id: req.session.user.id,
-              match_two_user_id: req.params.id
-            }).then(() => {
-              res.redirect('/dashboard')
-            })
           }
         }
-      } else {
+      }
+      if (!alreadyMatch) {
         knex('connections').insert({
           match_one_user_id: req.session.user.id,
           match_two_user_id: req.params.id
